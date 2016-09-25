efccf65db2b6ba122d5827d779f8619d5e0c1da3
Göran Broström <goran.brostrom@umu.se>
2016-09-25 22:14:13 +0200

    update

--- R/plotED.glm.R
+++ R/plotED.glm.R
@@ -2,7 +2,8 @@ plotED.glm <- function(res,
                        col = 1:length(res),
                        logScale = FALSE,
                        ylab = "",
-                       main = ""){
+                       main = "",
+                       exclude = 100){
     
     ## 'res' is a list of matrices of cumulative hazards from a piecewise-contant fit
     ## We want the last column of each matrix.
@@ -13,7 +14,9 @@ plotED.glm <- function(res,
     k <- NCOL(res[[1]])
     m <- NROW(res[[1]])
     labb <- names(res)
-    
+
+    out <- 1:nper
+    out <- out[!(out %in% exclude)]
     outp <- matrix(0, ncol = nper, nrow = m)
 
     for (i in 1:nper){
@@ -28,18 +31,20 @@ plotED.glm <- function(res,
     xv <- (as.numeric(substr(names(res), 1, 4)) +
         as.numeric(substr(names(res), 6, 9))) / 2
 
-    plot(xv, outp[1, ], type = "b", lty = 2, col = col[1], pch = 1,
+    plot(xv[out], outp[1, -exclude], type = "b", lty = 2, col = col[1], pch = 1,
          ylim = c(fr, to), main = main,
          axes = FALSE, ylab = ylab, cex.axis = 0.4, xlab = "", 
          xlim = c(min(xv), max(xv) + 10))
     axis(1, at = xv, lab = labb, las = 2)
     axis(2, las = 2)
     box()
+    ##lines(xv[7:nper], outp[1, 7:nper], col = col[1], pch = 1)
     for (i in 2:m){
-        points(xv, outp[i, ], col = col[i], pch = i)
-        lines(xv, outp[i, ], col = col[i], lty = i)
+        points(xv[out], outp[i, out], col = col[i], pch = i)
+        lines(xv[out], outp[i, out], col = col[i], lty = i)
+        ##lines(xv[7:nper], outp[i, 7:nper], col = col[i], lty = i)
     }
-    abline(v = xv, lty = 3, col = "magenta")
+    abline(v = xv[out], lty = 3, col = "magenta")
     abline(h = 0)
     legend("topright", legend = 1:m, col = col, pch = 1:m, lty = 1:m)
     invisible(outp)
