ac4ceb37f0ce9a9850857598a290a99fb159c9e6
Göran Broström <goran.brostrom@umu.se>
2016-11-01 17:08:44 +0100

    fix errors with cancer causes

--- R/getCancer.R
+++ R/getCancer.R
@@ -1,23 +1,30 @@
 getCancer <- function(dat){
     ## 'dat' is 'vb'
-    dat$eb <- dat$exit + dat$birthdate
-    cancer <- with(dat, !is.na(ULORSAK) &
-                       event == 1 &
-                       ((eb < 1951) & ULORSAK == 12 |
-                            (eb >= 1969 & eb < 1997 & ULORSAK %in% 140:239) |
-                            (eb >= 1997 & eb < 2014 & ULORSAK == "C")))
-    dat$cancer <- cancer
-    dat$eb <- NULL
-    dat
-}
+    ##dat$eb <- dat$exit + dat$birthdate
+    ##cancer <- with(dat, !is.na(ULORSAK) &
+    ##                   event == 1 &
+    ##                   ((eb < 1951) & ULORSAK == 12 |
+    ##                        (eb >= 1969 & eb < 1997 & ULORSAK %in% 140:239) |
+    ##                        (eb >= 1997 & eb < 2014 & ULORSAK == "C")))
+    ##dat$cancer <- cancer
+    ##dat$eb <- NULL
+    ##dat
+#}
 ## New version: 'ULORSAK' is a character vector:
     library(stringr)
     eb <- dat$exit + dat$birthdate
     ##cancer <- logical(NROW(dat))
+    
     ulor <- str_trim(dat$ULORSAK)
     ## First, fix ulor start with "C":
     fc <- (substr(ulor, 1, 1) == "C") & (eb >= 1997) & (eb < 2014)
     cancer <- fc
     ## Then the 'numeric' versions:
     ##ulor <- as.numeric(ulor)  # NA if letters in it
-    
+    fc <- (ulor %in% as.character(140:239)) & (eb >= 1969) & (eb < 1997)
+    cancer <- cancer | fc
+    fc <- (ulor == "12") & (eb < 1951)
+    cancer <- cancer | fc
+    dat$cancer <- dat$event & cancer
+    dat
+}
