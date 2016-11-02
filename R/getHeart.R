9eccfd11eabd2832114232ea431e9a4409dfd525
Göran Broström <goran.brostrom@umu.se>
2016-11-02 22:09:22 +0100

    fix NA

--- R/getHeart.R
+++ R/getHeart.R
@@ -15,13 +15,13 @@ getHeart <- function(dat){
     eb <- dat$exit + dat$birthdate
     library(stringr)
     ulor <- str_trim(dat$ULORSAK)
-    fc <- (substr(ulor, 1, 1) %in% c("I", "J")) & (eb >= 1997) & (eb < 2014)
+    fc <- (!is.na(ulor)) & (substr(ulor, 1, 1) %in% c("I", "J")) & (eb >= 1997) & (eb < 2014)
     heart <- fc
-    fc <- (ulor %in% as.character(390:459)) & (eb >= 1987) & (eb < 1997)
+    fc <- (!is.na(ulor)) & (ulor %in% as.character(390:459)) & (eb >= 1987) & (eb < 1997)
     heart <- heart | fc
-    fc <- (ulor %in% as.character(390:458)) & (eb >= 1969) & (eb < 1987)
+    fc <- (!is.na(ulor)) & (ulor %in% as.character(390:458)) & (eb >= 1969) & (eb < 1987)
     heart <- heart | fc
-    fc <- (ulor == "19") & (eb < 1951)
+    fc <- (!is.na(ulor)) & (ulor == "19") & (eb < 1951)
     heart <- heart | fc
     dat$heart <- dat$event & heart
     dat
