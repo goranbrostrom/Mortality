ced6878f099ef1c1c3ebd21f3fa74e9ee462bb5c
Göran Broström <goran.brostrom@umu.se>
2016-11-07 10:55:46 +0100

    find more cardioascular deaths

--- R/getHeart.R
+++ R/getHeart.R
@@ -14,12 +14,17 @@ getHeart <- function(dat){
     ## New version (ULORSAK is a character vector)
     eb <- dat$exit + dat$birthdate
     library(stringr)
-    ulor <- str_trim(dat$ULORSAK)
+    ulor <- str_trim(as.character(dat$ULORSAK))
     fc <- (!is.na(ulor)) & (substr(ulor, 1, 1) %in% c("I", "J")) & (eb >= 1997) & (eb < 2014)
     heart <- fc
-    fc <- (!is.na(ulor)) & (ulor %in% as.character(390:459)) & (eb >= 1987) & (eb < 1997)
+    fc <- (!is.na(ulor)) & (substr(ulor, 1, 3) %in% as.character(390:459)) & 
+        (eb >= 1987) & (eb < 1997)
     heart <- heart | fc
-    fc <- (!is.na(ulor)) & (ulor %in% as.character(390:458)) & (eb >= 1969) & (eb < 1987)
+    fc <- (!is.na(ulor)) & (substr(ulor, 1, 3) %in% as.character(390:458)) & 
+        (eb >= 1969) & (eb < 1987)
+    heart <- heart | fc
+    fc <- (!is.na(ulor)) & (substr(ulor, 1, 3) %in% as.character(400:468)) & 
+        (eb >= 1960) & (eb < 1969)
     heart <- heart | fc
     fc <- (!is.na(ulor)) & (ulor == "19") & (eb < 1951)
     heart <- heart | fc
