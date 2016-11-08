f5bfdf4629a49b98a0b83a375ce8f2b78978ca47
Göran Broström <goran.brostrom@umu.se>
2016-11-08 22:17:08 +0100

    in again

--- R/plotWEduc.R
+++ R/plotWEduc.R
@@ -6,14 +6,19 @@ plotWEduc <- function(fit){
     educ <- co[substr(names(co), 1, 4) == "educ"]
     income <- co[substr(names(co), 1, 4) == "inco"]
 
-    educ <- exp(c(con, educ + con))
+    educ <- exp(c(0, educ))
+    ##educ <- exp(c(con, educ + con))
     names(educ) <- 1:7
-    income <- exp(c(con, income + con))
+    income <- exp(c(0, income))
+    ##income <- exp(c(con, income + con))
     names(income) = c("lowest", "low", "high", "highest")
+    base <- co[1:5]
+    base <- c(co[1], co[1] + co[2:5])
+    mort <- 5 * sum(exp(base))
     oldpar <- par(mfrow = c(1, 2))
-    barplot(100 * educ, xlab = "Education", col = "green", las = 1, 
+    barplot(100 * (1 - exp(-educ * mort)), xlab = "Education", col = "green", las = 1, 
             ylab = "Mortality per cent")
-    barplot(100 * income, xlab = "Income", col = "blue", las = 2,
+    barplot(100 * (1 - exp(-income * mort)), xlab = "Income", col = "blue", las = 2,
             ylab = "Mortality per cent")
     par(oldpar)
     invisible(list(educ = educ, income = income))
