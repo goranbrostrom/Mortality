001ebc7f0326cbf016fbfd08360a7df008c669ff
Göran Broström <goran.brostrom@umu.se>
2017-05-18 16:42:34 +0200

    refresh

--- R/plotHaz.glm.R
+++ R/plotHaz.glm.R
@@ -1,10 +1,13 @@
-plotHaz.glm <- function(res, printLegend = FALSE, col = 1:4,
+plotHaz.glm <- function(res, printLegend = FALSE, col = c("black", "blue", "red"),
                     xlab = "", main = "", layout = c(4, 3)){
     ## 'res' is list of results from a fit.
     ## Note, number of levels of hisclass is hardcoded to 4 (four) here!!
     ## Will be fixed.
     
-    leg <- c("elite", "lowMan", "lowWhiteC", "worker") ## a hack for the time being
+    ##leg <- c("elite", "lowMan", "lowWhiteC", "worker") ## a hack for the time being
+    
+    leg <- rownames(res[[1]])
+    n.leg <- length(leg)
     
     oldpar <- par(mfrow = layout)
     on.exit(par(oldpar))
@@ -24,7 +27,7 @@ plotHaz.glm <- function(res, printLegend = FALSE, col = 1:4,
         plot(x, y[1, ], col = col[1], type = "l", lty = 1,
               main = names(res)[i], ylim = c(0, ymax),
              xlab = "Age", ylab = "Cum. Hazards", lwd = 2)
-        for (j in 2:4){
+        for (j in 2:n.leg){
             lines(x, y[j, ], col = col[j], lty = j, lwd = 2)
         }
         legend("topleft", legend = leg, col = col, lty = 1:4, cex = 1.0)
