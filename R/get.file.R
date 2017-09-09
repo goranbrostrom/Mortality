e487a70c3b1a71923430d179195701ee04abe1d9
Göran Broström <goran.brostrom@umu.se>
2017-09-09 22:42:49 +0200

    need internet connection

--- R/get.file.R
+++ R/get.file.R
@@ -1,12 +1,12 @@
 get.file <- function(name, link = "http://capa.ddb.umu.se/data/"){
     filename = paste("data/", name, ".rda", sep = "")
-    if (file.exists(filename)){
-        load(filename)
-    }else{
+    ##if (file.exists(filename)){
+      ##  load(filename)
+    ##}else{
         con <- url(paste(link, name, ".rda", sep = ""))
         load(con)
         close(con)
-        save(list = name, file = filename)
-    }
+        ##save(list = name, file = filename)
+    ##}
     get(name)
 }
