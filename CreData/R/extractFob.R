e379d0c3eea422f237e95ed7ab1747474bc8a50f
Göran Broström <goran.brostrom@umu.se>
2018-03-03 20:11:20 +0100

    continuing

--- CreData/R/extractFob.R
+++ CreData/R/extractFob.R
@@ -1,26 +1,15 @@
 extractFob <- function(year, followUp, data){
-    ## data = fob
-    out <- fob[fob$AR == year & 
-                     fob$KOMMUN %in% ourkom &
-                     fob$LINNEID %in% per$LINNEID, ]
-    ```
+    ## data = fob (or lisa?)
     
-    They need to be of the right age (born on November 1, 1940 or earlier). We
-    get the birthdates from *inddata*.
+    out <- data[data$AR == year, ]
     
-    ```{r mat60}
     library(eha)
-    indx <- match(fob60$LINNEID, per$LINNEID)
-    fob60$birthdate <- per$birthdate[indx]
-    fob60$fobdate <- as.numeric(toTime("1960-11-01"))
-    ##lim <- as.numeric(toTime("1940-11-01"))
-    ##fob60 <- fob60[fob60$birthdate < lim, ]
-    indx <- match(fob60$LINNEID, per$LINNEID) # NOTE: Necessary!
-    fob60$enter <- fob60$fobdate - fob60$birthdate
-    dd <- per$deathdate[indx]
-    ##fob60$event <- !(is.na(dd)) & (dd <= lim + 25) # NOTE: 40 --> 25!
-    fob60$event <- !(is.na(dd)) & (dd <= fob60$fobdate +5)
-    fob60$exit <- fob60$enter + 5
-    fob60$exit[fob60$event] <- dd[fob60$event] - fob60$birthdate[fob60$event]
-    head(fob60)
+    
+    out$fobdate <- as.numeric(toTime(paste(year, "11", "01", sep = "-")))
+    
+    out$enter <- out$fobdate - out$birthdate
+    out$event <- !(is.na(out$deathdate)) & (out$deathdate <= out$fobdate + followUp)
+    out$exit <- out$enter + followUp
+    out$exit[out$event] <- out$deathdate[out$event] - out$birthdate[out$event]
+    out
 }
